02785ec5c13d8e96965528ddd367961ac76be639
CodingQ92 <codingrepo@163.com>
2024-11-28 00:16:54 +0800

    :wrench: 修改配置以便页面访问

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,14 +3,14 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+      base: "/blog/", // 部署到 GitHub Pages 时需要加上配置的后缀
 
-  lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+      lang: "zh-CN",
+      title: "博客演示",
+      description: "vuepress-theme-hope 的博客演示",
 
-  theme,
+      theme,
 
-  // 和 PWA 一起启用
-  // shouldPrefetch: false,
+      // 和 PWA 一起启用
+      // shouldPrefetch: false,
 });
