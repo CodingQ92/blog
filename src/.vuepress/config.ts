141c1b61d114425c0433ab3bd3be0200f19d23ff
CodingQ92 <codingrepo@163.com>
2024-11-30 21:08:39 +0800

    :tada: 部分替换

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,13 +4,10 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
       base: "/blog/", // 部署到 GitHub Pages 时需要加上配置的后缀
-
       lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
-
+      title: "CodingQ92",
+      description: "CodingQ92博客站",
       theme,
-
       // 和 PWA 一起启用
       // shouldPrefetch: false,
 });
